1e3f3f46ec34969e764d33b1159c96c2bdc2e4b2
Andrej Kováč <93122145+BassBoster@users.noreply.github.com>
2021-11-12 16:40:25 +0100

    vypracované

--- Open-Lab-01.04/Triangle.cs
+++ Open-Lab-01.04/Triangle.cs
@@ -7,6 +7,12 @@ namespace Open_Lab_01._04
         public double Area(double foundation, double height)
         {
             throw new NotImplementedException();
+            Console.WriteLine("zadaj výšku");
+            double viska = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("zadaj stranu");
+            double strana = Convert.ToDouble(Console.ReadLine());
+            double vipocet = strana * viska / 2;
+            Console.WriteLine("vísledok " + vipocet);
         }
     }
 }
